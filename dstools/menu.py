7d48efe8ae6469086e25a05716645af11014a13b
Isaac Faber <isaacfab@gmail.com>
2023-10-16 20:52:05 -0400

    tweaking commands

--- dstools/menu.py
+++ dstools/menu.py
@@ -1,4 +1,3 @@
-import docker, click, webbrowser, requests, json, time, re, os, sys, randomfrom operator import itemgetterfrom pathlib import Pathfrom simple_term_menu import TerminalMenu  
-import docker, click, webbrowser, requests, json, time, re, os, sys, randomfrom operator import itemgetterfrom pathlib import Pathfrom simple_term_menu import TerminalMenu  
+
 def menu():
     dsToolTerminalMenu()
